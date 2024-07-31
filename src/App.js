276630c13900565999bc3f4e960cdbeaf6010368
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-07-31 12:41:26 +0530

    commit 26

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 
 import UserProfiles from './components/UserProfiles'
-import event from './components/Event'
+import Event from './components/Event'
 
 function App() {
   return (

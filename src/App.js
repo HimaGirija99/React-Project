7f88ff6ea2356bbe66ee414b309e288b896796fd
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-07-31 12:04:02 +0530

    commit 15

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div> <h1>Hello from React</h1> 
       <MyComponent/>
-      <UserProfiles Name = "John Doe" Age = "25" Occupation = "gardener"/>
+      <UserProfiles Name = "John Doe" Age = {25} Occupation = "gardener"/>
       </div>
   )
 }

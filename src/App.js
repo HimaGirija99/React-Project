c9fad144e2e9e92073525c03594955938e27418b
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-07-31 11:25:23 +0530

    commit 5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import MyComponent from './components/MyComponent'
+import UserProfile from './componentUserProfiles'
 
 function App() {
   return (
     <div> <h1>Hello from React</h1> 
       <MyComponent/>
+      <UserProfile name = "John Doe" age = "25" occupation = "gardener"/>
       </div>
   )
 }
 
-    export default App;
+export default App;

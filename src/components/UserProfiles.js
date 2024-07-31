97e2ad738111b7692c566d1b9a7a980ca93fcd25
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-07-31 12:12:54 +0530

    commit 19

--- src/components/UserProfiles.js
+++ src/components/UserProfiles.js
@@ -5,7 +5,7 @@ function UserProfiles(props){
     return (
         <div>
             <h3>
-                Name:{props.name}, Age:{props.age}, Occupation:{props.occupation}
+                Name:{props.name} Age:{props.age} Occupation:{props.occupation}
             </h3>
         </div>
     )

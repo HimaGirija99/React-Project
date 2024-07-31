a7e8261ac7e6c00bb81d2a13e3802f85c0e3aee5
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-07-31 14:51:57 +0530

    commit 38

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,21 +2,11 @@ import {useState} from 'react'
 import React from 'react'
 function Counter(){
     const[count, setCount] = useState(0)
-    function increment(){
-        setCount(count + 1)
-    }
-    function decrement(){
-        if(count === 0){
-            setCount(count)
-        }else{
-            setCount(count - 1)
-        }
-        
-    }
+    
     return (
         <div><h2>The count value is {count} </h2>
-        <button OnClick={increment}> Increment </button>
-        <button OnClick={decrement}> Decrement </button> 
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
         </div>
     
     )

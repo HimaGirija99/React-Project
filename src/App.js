4c414a7e5a57f2561fb5dc635653a693070ec37c
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-07-31 12:41:49 +0530

    commit 27

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       
       <UserProfiles Name = "John Doe" Age = {25} Occupation = "gardener"/>
       <UserProfiles Name = "Steve" Age = {35} Occupation = "lawyer"/>
-      <event/>
+      <Event/>
       </div>
   )
 }

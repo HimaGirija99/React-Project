ea6aeb0263e0b6996ec25ecdbdaef6bca6a07275
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-07-31 11:58:02 +0530

    commit 13

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
 import MyComponent from './components/MyComponent'
-import UserProfile from './components/UserProfiles'
+import UserProfiles from './components/UserProfiles'
 
 function App() {
   return (
     <div> <h1>Hello from React</h1> 
       <MyComponent/>
-      <UserProfile Name = "John Doe" Age = "25" Occupation = "gardener"/>
+      <UserProfiles Name = "John Doe" Age = "25" Occupation = "gardener"/>
       </div>
   )
 }

0630d7e3a9d7399def15a2c9b1276f4fdbc4100c
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-01 12:51:04 +0530

    commit 55

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-import  Form from './components/Form.js'
+import  Ue from './components/Ue.js'
 
 
 function App() {
   return (
     <div>       
-      <Form/>
+      <Ue/>
       
       </div>
   )
---
+++ src/components/Ue.js
@@ -0,0 +1,22 @@
+import {useEffect, useState} from 'react'
+
+function Ue(){
+    const[count, setCount] = useState(0)
+    let incrementCount = () =>{
+        setCount(count+1)
+    }
+    useEffect(()=>{
+        console.log('use Effect runs')
+        document.title = 'Button clicked for {count} times'
+    })
+    console.log('other code that gets executed')
+    return (
+        <div>
+            <h1>
+                This is my count value:{count}
+                <button onClick={incrementCount}></button>
+            </h1>
+        </div>
+    )
+}
+export default Ue

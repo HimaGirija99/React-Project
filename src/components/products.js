d7e2f718d1605186827d757bb88807ce2f101b3b
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-01 11:07:17 +0530

    commit 42

--- src/components/products.js
+++ src/components/products.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function products() {
     //let products =['laptop', 'mobile', 'tablet', 'camera', 'watch', 'tv', 'headphones', 'speaker']
-    let productsList = [{id:1, product:'laptop', price:50000}, {id:2, product:'mobile', price:40000}, {id:3, product:'tablet', price:35000}, {id:4, product:'camera', price:60000},
-        {id:5, product:'watch', price:5000}, {id:6, product:'tv', price:40000}, {id:7, product:'headphones', price:3000}, {id:8, product:'speaker', price:6000}
+    let productsList = [{id:1, name:'laptop', price:50000}, {id:2, name:'mobile', price:40000}, {id:3, name:'tablet', price:35000}, {id:4, name:'camera', price:60000},
+        {id:5, name:'watch', price:5000}, {id:6, name:'tv', price:40000}, {id:7, name:'headphones', price:3000}, {id:8, name:'speaker', price:6000}
     ]
   return (
     // <div>
@@ -13,7 +13,7 @@ function products() {
     // </div>
     <div>
         {productsList.map((product)=>{
-            return <li key={product.id}>{product}:{product.price}</li>
+            return <li key={product.id}>{product.name}:{product.price}</li>
         })}
     </div>
   )

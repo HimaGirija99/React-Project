1270ecfaa0f550976e31df59981db5dd2add7b30
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-01 12:02:45 +0530

    commit 47

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,20 +3,27 @@ import { useState } from 'react'
 
 function Form() {
     const[name, setName] = useState('')
-    let handleChange =(e) =>{               
+    const[age, setAge] = useState(0)
+    let handleName =(e) =>{               
         setName(e.target.value)
+        
+    let handleAge =(e) =>{         
+        setAge(e.target.value)
     }
     let handleSubmit=(e)=>{
         e.preventDefault()
         console.log(name)
+        console.log(age)
     }
   return (
     <div>
         <h1>This is a form</h1>
         <form onSubmit={handleSubmit}>
             <label>Name</label>
-            <input onChange = {handleChange} type='text' value={name}></input>
-            <button>Submit Form</button>
+            <input onChange = {handleName} type='text' value={name}></input>
+            <button>Submit Name</button>
+            <input onChange = {handleAge} type='text' value={name}></input>
+            <button>Submit Age</button>
         </form>
     </div>
   )

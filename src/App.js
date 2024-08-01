30197300b25cc7627c244732b5cfa635ef252336
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-01 10:31:10 +0530

    commit 36

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@
 import './App.css';
 import './components/products.js';
 
-import Counter from './components/Counter'
+
 function App() {
   return (
     <div>       
       <products/>
-      <Counter/>
+      
       </div>
   )
 }
